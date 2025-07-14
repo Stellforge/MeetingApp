86d4d1b55b670e3bebef3b3f37555aed863330db
Furkan Erçelebi <stellforge6@gmail.com>
2025-07-14 15:07:45 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using MeetingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int saat = DateTime.Now.Hour;
+            //ViewBag.selamlama = saat > 12 ? "İyi Günler" : "Günaydın";
+            //ViewBag.Username = "Furkan Ercelebi";
+            //ViewData["Username"] = "Furkan Ercelebi";
+
+            ViewData["Selamlama"]= saat > 12 ? "İyi Günler" : "Günaydın";
+            int UserCount = Repostiry.Users.Where(i => i.WillAttend == true).Count();
+            var meetingInfo  = new MeetingInfo()
+            {
+            Id = 1,
+            Location = "İstanbul, Abc Kongre Merkezi",
+            Date = new DateTime(2025, 01,20, 20, 0 ,0 ),
+            NumberOfPeople = UserCount,
+            };
+
+            
+            return View(meetingInfo);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    
+    }
+}
---
+++ Controllers/MeetingController.cs
@@ -0,0 +1,52 @@
+﻿using MeetingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+    public class MeetingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Apply(UserInfo model)
+        {
+            if (ModelState.IsValid) 
+            { 
+            Repostiry.CreatedUser(model);
+            ViewBag.UserCount = Repostiry.Users.Where(i => i.WillAttend == true).Count();
+            return View("Thanks", model);
+            }
+            else
+            {
+                return View(model);   
+                    }
+        }
+
+        public IActionResult Thanks()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View(Repostiry.Users);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(Repostiry.GetById(id));
+        }
+
+
+    }
+}
